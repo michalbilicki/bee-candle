f8a63c6cac61b41e2f4a4aadf48c3784e1dc2e99
Michał Bilicki <michalbilicki@Micha-Bilicki.local>
2022-11-15 17:23:29 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import beeCandleLogo from 'assets/bee-candle-logo.png'
+
+const App = () => (
+  <div className="app">
+    <header className="app-header">
+      <img src={beeCandleLogo} alt="Bee with the project name" />
+    </header>
+  </div>
+)
+
+export default App
